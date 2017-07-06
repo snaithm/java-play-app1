00bb64df9dca8c89d2a3ebfe143ba96a23f53047
Matthew Snaith <mathswan1984@gmail.com>
2017-07-06 09:28:18 +0100

    pom file added along with inintial test to validate person model

--- test/models/PersonTests.java
+++ test/models/PersonTests.java
@@ -1,7 +1,30 @@
 package models;
 
-/**
- * Created by snaithm on 06/07/17.
- */
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import org.junit.Before;
+import org.junit.Test;
+
 public class PersonTests {
+
+    private static Validator validator;
+
+    @Before
+    public void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+    @Test
+    public void PersonModel_HappyPath() {
+        Person person = new Person();
+        person.setFirstname("Test first name");
+        person.setLastname("Test last name");
+        Set<ConstraintViolation<Person>> violations = validator.validate(person);
+        assertTrue(violations.isEmpty());
+    }
 }
