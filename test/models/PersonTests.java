ce75c0c390b2b90fec5168df9b68f341c033c572
Matthew Snaith <mathswan1984@gmail.com>
2017-07-06 09:41:40 +0100

    further test added

--- test/models/PersonTests.java
+++ test/models/PersonTests.java
@@ -1,7 +1,6 @@
 package models;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import java.util.Set;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
@@ -13,18 +12,27 @@ import org.junit.Test;
 public class PersonTests {
 
     private static Validator validator;
+    private Person person = new Person();
 
     @Before
     public void setUp() {
+        person.setFirstname("Test first name");
+        person.setLastname("Test last name");
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
     }
+
     @Test
-    public void PersonModel_HappyPath() {
-        Person person = new Person();
-        person.setFirstname("Test first name");
-        person.setLastname("Test last name");
+    public void PersonModel_HappyPath() { //default values used from setup
         Set<ConstraintViolation<Person>> violations = validator.validate(person);
-        assertTrue(violations.isEmpty());
+        assertEquals(0, violations.size());
+    }
+
+    @Test
+    public void PersonModel_SadPath_FirstnameEmptyString() {
+        person.setFirstname("");
+        Set<ConstraintViolation<Person>> violations = validator.validate(person);
+        assertEquals(1, violations.size());
+        assertEquals("Please enter a first name(s)", violations.iterator().next().getMessage());
     }
 }
