9032c0584c5bd614098bbf3ad263798bf6081807
Matthew Snaith <mathswan1984@gmail.com>
2017-07-06 07:06:50 +0100

    error string passed to welcome view

--- app/controllers/Home.java
+++ app/controllers/Home.java
@@ -12,14 +12,14 @@ public class Home extends Controller {
     public Result index() {
 
         Form<Person> personForm = formGen.form(Person.class);
-        return ok(welcome.render(personForm));
+        return ok(welcome.render(personForm, null));
     }
 
     public Result submit() {
 
         Form<Person> personForm = formGen.form(Person.class).bindFromRequest();
         if (personForm.hasErrors()) {
-            return ok(welcome.render(personForm));
+            return ok(welcome.render(personForm, "Correct the errors below to continue"));
         }
 
         return ok(success.render(personForm.get()));
