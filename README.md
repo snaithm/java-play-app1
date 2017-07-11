ebe92373b6495b2c6c03f4d0ae067da80319cd0d
Matthew Snaith <mathswan1984@gmail.com>
2017-07-11 13:12:45 +0100

    s3 update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Simple Java Play Application
-A simple MVC application built using Java with Play Framework
+A small MVC application built using Java with Play Framework interacting with an AWS S3 bucket
+
+Connection credentials are not stored in the repo
--- app/controllers/Home.java
+++ app/controllers/Home.java
@@ -11,13 +11,18 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class Home extends Controller {
 
@@ -33,24 +38,37 @@ public class Home extends Controller {
         if (userForm.hasErrors()) {
             return badRequest(welcome.render(userForm, validationErrorMsg));
         }
-        User user = userForm.get();
+
         try {
-            uploadUserDetailsToS3(user);
+            AWSCredentials credentials = new ProfileCredentialsProvider().getCredentials();
+            AmazonS3 s3client = new AmazonS3Client(credentials);
+            if (usernameExists(userForm.get().getUsername(), s3client)) {
+                return badRequest(welcome.render(userForm, usernameExistsMsg));
+            }
+            uploadUserDetailsToS3(userForm.get(), s3client);
         }
         catch (AmazonClientException ae)
         {
             return internalServerError(welcome.render(userForm, serverErrorMsg));
         }
+
         return ok(success.render(userForm.get()));
     }
 
-    private void uploadUserDetailsToS3(User user) {
+    private void uploadUserDetailsToS3(User user, AmazonS3 s3client) {
 
-        AWSCredentials credentials = new ProfileCredentialsProvider().getCredentials();
-        AmazonS3 s3client = new AmazonS3Client(credentials);
-        String fileName = String.format("%s-%s-registration.txt", user.getFirstname().toLowerCase(), user.getLastname().toLowerCase());
+        String fileName = String.format("%s.txt", user.getUsername());
         ByteArrayInputStream content = new ByteArrayInputStream(createContent(user).getBytes());
         s3client.putObject(new PutObjectRequest(bucketName, fileName, content , new ObjectMetadata()));
+    }
+
+    private Boolean usernameExists(String username , AmazonS3 s3client) {
+        List<S3ObjectSummary>bucketItems =  s3client.listObjects(bucketName).getObjectSummaries();
+        for (S3ObjectSummary item : bucketItems) {
+            String itemName = item.getKey().replace(".txt","");
+            if (itemName.equals(username)) return true;
+        }
+        return false;
     }
 
     private String createContent(User user) {
@@ -58,13 +76,14 @@ public class Home extends Controller {
         DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date today = Calendar.getInstance().getTime();
         String nowString = df.format(today);
-        return String.format("%s %s accepted t&c on %s", user.getFirstname().toLowerCase(), user.getLastname().toLowerCase(), nowString);
+        return String.format("%s %s %s accepted t&c on %s", user.getUsername(), user.getFirstname(), user.getLastname(), nowString);
     }
 
     @Inject
     FormFactory formGen;
 
-    private final String bucketName = "snaithm-testapp1";
+    private final String bucketName = "pf-testapp1";
     private final String validationErrorMsg = "Correct the errors below to continue.";
     private final String serverErrorMsg = "An error has occurred saving your registration details. The data store could not be reached.";
+    private final String usernameExistsMsg = "Your chosen username already exists. Please enter a different username";
 }
